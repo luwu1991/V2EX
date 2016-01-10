4284dca736e6c0a731b17e0e15da57c8d4de1137
wulu <334131038@qq.com>
2016-01-10 22:04:38 +0800

    实验用UICollectionView进行分页显示

--- V2EX/V2EX/Classify/ClasslfyViewController.m
+++ V2EX/V2EX/Classify/ClasslfyViewController.m
@@ -13,20 +13,47 @@
 #import "NewestTableViewCell.h"
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "NewestDateilViewController.h"
-@interface ClasslfyViewController ()
+
+static NSString *ClasslfyCellSection1 = @"ClasslfyCellSection1";
+static NSString *ClasslfyCellSection2 = @"ClasslfyCellSection2";
+@interface ClasslfyViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 @property(nonatomic,strong)NSMutableArray *classilyArray;
 @property(nonatomic,strong)NSMutableDictionary *classilyDict;
 @property(nonatomic,strong)NSMutableArray *itemArray;
+@property(nonatomic,strong)UITableView *tableView;
+@property(nonatomic,strong)UICollectionView *collectionView;
 @end
 
 @implementation ClasslfyViewController
 
+-(UICollectionView *)collectionView{
+    if (_collectionView == nil) {
+        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc]init];
+        flowLayout.itemSize = CGSizeMake(self.view.width, self.view.height - 44 - 64 - 49);
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+        flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        flowLayout.minimumInteritemSpacing = 0;
+        flowLayout.minimumLineSpacing = 0;
+        _collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 44+64, self.view.width, flowLayout.itemSize.height) collectionViewLayout:flowLayout];
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        _collectionView.pagingEnabled = YES;
+        _collectionView.bounces = NO;
+        _collectionView.backgroundColor = [UIColor redColor];
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:ClasslfyCellSection1];
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:ClasslfyCellSection2];
+        [self.view addSubview:_collectionView];
+    }
+    return _collectionView;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"类别";
     
     self.itemArray = [NSMutableArray array];
-    self.listTableView.frame = CGRectMake(0, 108, self.view.width, self.view.height - 152);
+    self.automaticallyAdjustsScrollViewInsets = false;
+//    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 0, 0) style:UITableViewStylePlain];
+//    self.tableView.frame = CGRectMake(0,44+64, self.view.width, self.view.height - 44 - 64 - 49);
     self.classilyArray = [NSMutableArray arrayWithArray:@[@"技术",@"创意",@"好玩",@"Apple",@"酷工作",@"交易",@"城市",@"问与答",@"最热",@"全部",@"R2"]];
     self.classilyDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
                          @"tech",@"技术",
@@ -41,21 +68,44 @@
                          @"all",@"全部",
                          @"r2",@"R2",nil];
     
-//    [self listTableView];
-    [self.listTableView registerNib:[UINib nibWithNibName:@"NewestTableViewCell" bundle:nil] forCellReuseIdentifier:@"NewestCell"];
+//    self.tableView.delegate = self;
+//    self.tableView.dataSource = self;
+//    [self.tableView registerNib:[UINib nibWithNibName:@"NewestTableViewCell" bundle:nil] forCellReuseIdentifier:@"NewestCell"];
 //    self.listTableView.estimatedRowHeight = 80;
-    self.listTableView.rowHeight = 90;
-    [self.view addSubview:self.listTableView];
-//    [self.listTableView removeFromSuperview];
+    
+    
+//    [self.view addSubview:self.tableView];
+
+    [self collectionView];
+    [self setTableVIeWithTag];
     [self loadDataFromWeb];
-    // Do any additional setup after loading the view.
+   
+}
+
+-(UIView*)setTableVIeWithTag{
+//    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 0, 0) style:UITableViewStylePlain];
+//    self.tableView.frame = CGRectMake(0,0, self.view.width, self.collectionView.height);
+//    self.tableView.delegate = self;
+//    self.tableView.dataSource = self;
+//    [self.tableView registerNib:[UINib nibWithNibName:@"NewestTableViewCell" bundle:nil] forCellReuseIdentifier:@"NewestCell"];
+//    //    self.listTableView.estimatedRowHeight = 80;
+//    self.tableView.rowHeight = 90;
+//    return self.tableView;
+    UITableView *tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 0, 0) style:UITableViewStylePlain];
+    tableView.frame = CGRectMake(0,0, self.view.width, self.collectionView.height);
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [tableView registerNib:[UINib nibWithNibName:@"NewestTableViewCell" bundle:nil] forCellReuseIdentifier:@"NewestCell"];
+    //    self.listTableView.estimatedRowHeight = 80;
+    tableView.rowHeight = 90;
+    return tableView;
 }
 
 -(void)loadDataFromWeb{
     [[LWHTTPManager shareManager]GetClassilyDataWith:V2ClassilyTypeJobs success:^(id data) {
         if (data) {
             self.itemArray = data;
-            [self.listTableView reloadData];
+            [self.collectionView reloadData];
         }
         else{
             [self showHint:@"没有数据"];
@@ -112,6 +162,38 @@
     next.theme = model;
     next.title = model.title;
     [self.navigationController pushViewController:next animated:YES];
+}
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 2;
+}
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 5;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ClasslfyCellSection1 forIndexPath:indexPath];
+        NSArray *array =cell.contentView.subviews;
+        for (UIView *sunView in array) {
+            [sunView removeFromSuperview];
+        }
+        [cell.contentView addSubview:[self setTableVIeWithTag]];
+        
+        NSLog(@"%ld",indexPath.row);
+        return cell;
+    }
+    else{
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ClasslfyCellSection2 forIndexPath:indexPath];
+        NSArray *array =cell.contentView.subviews;
+        for (UIView *sunView in array) {
+            [sunView removeFromSuperview];
+        }
+        [cell.contentView addSubview:[self setTableVIeWithTag]];
+        
+        NSLog(@"%ld",indexPath.row);
+        return cell;
+    }
 }
 
 @end
